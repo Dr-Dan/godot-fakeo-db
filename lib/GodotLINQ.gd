e997450fc02f1da5e694c02122820c31e1fac4d8
Dr-Dan <danclarkeman@gmail.com>
2019-04-07 22:19:46 +0100

    first commit

---
+++ lib/GodotLINQ.gd
@@ -0,0 +1,142 @@
+#extends Resource
+class_name GodotLINQ
+
+class Comparer:
+	func eval(item):
+		return true
+	
+class cmp_get:
+	extends Comparer
+	var target
+	var val
+
+	func _init(target, val):
+		self.target = target
+		self.val = val
+
+	func eval(item):
+		for c in target:
+			if not c.eval(item):
+				return false
+		return true
+
+class cmp_and:
+	extends Comparer
+	var cmps
+	func _init(cmps):
+		self.cmps = cmps
+		
+	func eval(item):
+		for c in cmps:
+			if not c.eval(item):
+				return false
+		return true
+	
+class cmp_or:
+	extends Comparer
+	var cmps
+	func _init(cmps):
+		self.cmps = cmps
+		
+	func eval(item):
+		for c in cmps:
+			if c.eval(item):
+				return true
+		return false
+	
+class lt:
+	extends Comparer
+	var val
+	func _init(val):
+		self.val = val
+		
+	func eval(item):
+		return item < val
+	
+class gt:
+	extends Comparer
+	var val
+	func _init(val):
+		self.val = val
+		
+	func eval(item):
+		return item > val
+			
+class eq:
+	extends Comparer
+	var val
+	func _init(val):
+		self.val = val
+		
+	func eval(item):
+		return item == val
+			
+
+class IterList:
+	var start 
+	var curr
+	var end
+	var increment 
+	var items = []
+
+	func _init(items):
+		increment = 1
+		start = 0
+		curr = start
+		self.items = items
+		self.end = items.size()
+		
+	func size():
+		return items.size()
+
+	func is_done():
+		return (curr < end)
+
+	func do_step():
+		curr += increment
+		return is_done()
+
+	func _iter_init(arg):
+		curr = start
+		return is_done()
+
+	func _iter_next(arg):
+		return do_step()
+
+	func _iter_get(arg):
+		return items[curr]
+		
+	func where(cmps):
+		var result = []
+		for item in items:
+			for key in cmps:
+				if key in item:
+					if cmps[key].eval(item[key]):
+						result.append(item)
+		return IterList.new(result)
+		
+	func select(cmps, fields: Array):
+		var result = []
+		for item in items:
+			var n = {}
+			for key in cmps:
+				if key in item:
+					for f in fields:
+						if f in item\
+						and cmps[key].eval(item[key]):
+							n[f] = item[f]
+			if not n.empty():
+				result.append(n)
+		return IterList.new(result)
+		
+	func first(cmps):
+		var result = []
+		for item in items:
+			for key in cmps:
+				if key in item:
+					if cmps[key].eval(item[key]):
+						return item
+		return null
+		
+	func to_list(deep=false):
+		return items.duplicate(deep)
---
+++ lib/itertest.gd
@@ -0,0 +1,54 @@
+extends Node2D
+const GL = GodotLINQ
+
+class Human:
+	var name: String
+	var age: int
+	var addr_id: int
+	
+	func _init(name, age, addr_id):
+		self.name = name
+		self.age = age
+		self.addr_id = addr_id
+		
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var itr = GL.IterList.new([
+	{name="mike", age=22, addr_id=0}, 
+	{name="mindy", age=16, addr_id=1},
+	{name="trish", age=9, addr_id=2},
+	{name="aslan", age=199, addr_id=2},
+	Human.new("dan", 30, 0),
+	Human.new("ariel", 65, 3),
+	])
+	
+	var itr2 = GL.IterList.new([
+	{addr_id=0, street="dalesford"}, 
+	{addr_id=1, street="wendsleydale"},
+	{addr_id=2, street="londonberry"},
+	{addr_id=3, street="hubersville"},	
+	])
+
+#	var result = first.new({age=gt.new(70)}).eval(itr)
+#	var result2 = first.new({addr_id=eq.new(result.addr_id)}).eval(itr2)
+
+#	var sel_addr_id = select.new({age=gt.new(70)}, "addr_id")
+#	var sel_addr_info = first.new({addr_id=select.new(eq, "addr_id")})
+#	var result2 = connect(sel_addr_id, sel_addr_info).eval(items)
+		
+# for now, consider type checking inside gt, lt, eq
+#	could alo just return query from 'select' and run() it to get items
+# 	select.new({age=cmp_and.new([gt.new(20), lt.new(100)])}, ["addr_id", "name"])
+#   select.run(itr)?
+
+#	var result = itr.select({age=cmp_target.new(ai, gt)}, ["addr_id", "age"])
+	var age_comp = GL.cmp_and.new([GL.gt.new(20), GL.lt.new(100)])
+	var result = itr.select({age=age_comp}, ["addr_id", "name"])
+#	print(result.to_list())
+	
+	for i in result.to_list():
+		print(i.name, ", ", itr2.where({addr_id=GL.eq.new(i.addr_id)}).to_list())
+	
+#	var result = itr.first({age=gt.new(70)})
+#	var result2 = itr2.first({addr_id=eq.new(result.addr_id)})
+#	print(result,",",result2)	
